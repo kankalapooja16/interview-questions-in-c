8038426bb16ffc4c842b0be6c8fa2d0d0d803926
kankalapooja16 <kankalapooja16@gmail.com>
2026-02-03 16:26:17 +0530

    Add files via upload

---
+++ bricks.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+void main()
+{
+    int big;
+    int small;
+    int goal;
+    printf("enter the no of big bricks \n");
+    scanf("%d",&big);
+    printf("enter the no of small bricks \n");
+    scanf("%d",&small);
+    printf("enter the no of goal bricks \n");
+    scanf("%d",&goal);
+    int required=goal/5;
+    if(required<=big)
+    {
+        int rem=goal-(required*5);
+        if(rem<=small)
+    {
+        printf("true");
+    }
+    else
+{
+    printf("false");
+} 
+}
+else
+{
+    int rem=goal-(big*5);
+if(rem<=small)
+{
+    printf("true");
+}
+else
+{
+    printf("false");
+}
+}
+}
